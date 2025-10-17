02f6d4ad0d91dc6c34572013ad32096979e65189
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2025-10-17 23:09:36 +0700

    hotfix: footer

--- src/components/templates/FooterTemplates.tsx
+++ src/components/templates/FooterTemplates.tsx
@@ -9,11 +9,12 @@ export default function FooterTemplates(
       className="h-96 border-t-[1px] border-neutral-200/30 bg-black px-6 py-8"
       {...props}
     >
-    <div className="h-96 border-t-[1px] border-neutral-200/30 bg-black py-8 px-6">
-      <div className="">
-        <p className="text-md flex flex-col font-bold tracking-tighter text-white md:text-xl lg:text-3xl">
-          Fauziseptians <AuroraText>Software Developer</AuroraText>{' '}
-        </p>
+      <div className="h-96 border-t-[1px] border-neutral-200/30 bg-black px-6 py-8">
+        <div className="">
+          <p className="text-md flex flex-col font-bold tracking-tighter text-white md:text-xl lg:text-3xl">
+            Fauziseptians <AuroraText>Software Developer</AuroraText>{' '}
+          </p>
+        </div>
       </div>
     </div>
   );
