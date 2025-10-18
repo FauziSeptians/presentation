ce3af151bf9f3a25ada99979bd6b001bb8405c42
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2025-10-18 18:09:42 +0700

    hotfix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,42 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Portofolio',
-  description: 'Portofolio pribadi dengan gaya modern dan bersih',
+  title: 'Fauziseptians – Frontend Developer',
+  description:
+    'Portofolio digital Fauzi Septians, seorang frontend developer yang fokus pada desain modern, performa tinggi, dan pengalaman pengguna yang optimal.',
+  keywords: [
+    'muhammad fauzi septiana putra',
+    'Fauziseptians',
+    'Fauzi Septians',
+    'Frontend Developer',
+    'Web Developer',
+    'React',
+    'Next.js',
+    'Tailwind CSS',
+    'Portofolio Developer',
+    'UI Engineer',
+    'JavaScript',
+    'CSS',
+  ],
+  authors: [{ name: 'Fauzi Septians', url: 'https://fauziseptians.com' }],
+  creator: 'Fauzi Septians',
+  openGraph: {
+    title: 'Fauziseptians – Frontend Developer',
+    description:
+      'Lihat karya dan pengalaman Fauzi Septians sebagai frontend developer yang berfokus pada UI modern dan performa tinggi.',
+    url: 'https://fauziseptians.com',
+    siteName: 'Fauziseptians Portfolio',
+    images: [
+      {
+        url: 'https://fauziseptians.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Fauzi Septians Portfolio',
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
