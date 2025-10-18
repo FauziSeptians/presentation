1fac3eabd4e18fa524b29698c1c395386dc9ad33
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2025-10-18 12:54:34 +0700

    hotfix: nav val

--- src/components/atom/AnimatedSection.tsx
+++ src/components/atom/AnimatedSection.tsx
@@ -1,29 +1,25 @@
-'use client';
-
 import { motion, useInView } from 'framer-motion';
-import { HtmlHTMLAttributes, useRef } from 'react';
+import { useRef } from 'react';
+import type { HTMLMotionProps } from 'framer-motion';
 
 export default function AnimatedSection({
   children,
   className,
   ...props
-}: HtmlHTMLAttributes<HTMLDivElement> & {
-  children: React.ReactNode;
-}) {
+}: HTMLMotionProps<'section'>) {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true, margin: '0px 0px -100px 0px' });
 
   return (
-    <div {...props}>
-      <motion.div
-        ref={ref}
-        className={className}
-        initial={{ opacity: 0, y: 50 }}
-        animate={isInView ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 0.6, ease: 'easeOut' }}
-      >
-        {children}
-      </motion.div>
-    </div>
+    <motion.section
+      ref={ref}
+      className={className}
+      initial={{ opacity: 0, y: 50 }}
+      animate={isInView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.6, ease: 'easeOut' }}
+      {...props}
+    >
+      {children}
+    </motion.section>
   );
 }
