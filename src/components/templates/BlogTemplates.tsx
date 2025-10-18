baad9443dbaf486569571c56a463a500e2a0f259
Muhammad Fauzi Septiana Putra <114633811+FauziSeptians@users.noreply.github.com>
2025-10-18 10:33:54 +0700

    feat: pagination (#11)
    Co-authored-by: muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>

--- src/components/templates/BlogTemplates.tsx
+++ src/components/templates/BlogTemplates.tsx
@@ -2,30 +2,33 @@ import { HtmlHTMLAttributes } from 'react';
 import { AuroraText } from '../ui/aurora-text';
 import { HeroVideoDialog } from '../ui/hero-video-dialog';
 import blog from '../../data/blog-data.json';
+import Pagination from '../atom/Pagination';
 
 export default function BlogTemplates(
   props?: HtmlHTMLAttributes<HTMLDivElement>
 ) {
   return (
-    <div className="flex md:h-96 h-fit flex-col gap-12 px-6" {...props}>
+    <div className="flex h-fit flex-col gap-12 px-6 md:h-96" {...props}>
       <h1 className="flex justify-center gap-3 text-center text-4xl font-bold tracking-tighter text-white md:text-start md:text-5xl lg:text-7xl">
         My <AuroraText>Blog</AuroraText>{' '}
       </h1>
       <div className="h-full">
-        <div className="flex gap-4 md:flex-row flex-col">
-          {blog?.map((item) => {
-            return (
-              <HeroVideoDialog
-                key={item?.videoSrc}
-                className="!h-fit w-96"
-                animationStyle="from-center"
-                videoSrc={item?.videoSrc}
-                thumbnailSrc={item?.thumbnailSrc}
-                thumbnailAlt={item?.thumbnailSrc}
-              />
-            );
-          })}
-        </div>
+        <Pagination data={blog}>
+          {(data) => (
+            <div className="flex min-h-[220px] flex-col gap-4 md:grid md:grid-cols-3">
+              {data.map((item) => (
+                <HeroVideoDialog
+                  key={item.videoSrc}
+                  className="!h-full"
+                  animationStyle="from-center"
+                  videoSrc={item.videoSrc}
+                  thumbnailSrc={item.thumbnailSrc}
+                  thumbnailAlt={item.thumbnailSrc}
+                />
+              ))}
+            </div>
+          )}
+        </Pagination>
       </div>
     </div>
   );
