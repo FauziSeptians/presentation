b98e1686083e8352fe67e8ef6299428ffac28ecc
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2025-10-19 22:44:34 +0700

    hotfix: issue dark theme

--- src/components/molecules/SkillCard.tsx
+++ src/components/molecules/SkillCard.tsx
@@ -15,7 +15,7 @@ export default function SkillCard({
       className={cn(
         'relative h-24 w-64 cursor-pointer overflow-hidden rounded-xl border p-4',
         'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
-        'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
+        'border-gray-50/[.1] bg-gray-50/[.10] hover:bg-gray-50/[.15]'
       )}
     >
       <div className="flex h-full flex-row items-center gap-2">
@@ -27,12 +27,10 @@ export default function SkillCard({
           src={img}
         />
         <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
+          <figcaption className="text-sm font-medium text-white">
             {title}
           </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">
-            {description}
-          </p>
+          <p className="text-xs font-medium text-white/40">{description}</p>
         </div>
       </div>
     </figure>
